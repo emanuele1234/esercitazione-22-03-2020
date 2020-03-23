44afdc6dae92d5ce3a2e92c4e66280fe9acad609
Emanuele <sabetemanuele@gmail.com>
2020-03-23 10:59:11 +0100

    inserito codice che richiede informazioni personali

---
+++ main.py
@@ -0,0 +1,5 @@
+nome=input('inserisci il tuo nome ' )
+età= input('inserisci la tua età ')
+DoveVivi=input('inserisci la città in cui abiti ')
+Fam=input('inserisci il numero di persone nella tua famiglia ')
+print(" ciao " + nome + " hai " + età+ " anni "  + " vivi a " + DoveVivi + " nella tua famiglia siete " + Fam )
