027843151cb3e241ac5b4c8ba82fa51a445204a0
Emanuele <sabetemanuele@gmail.com>
2020-03-23 11:09:09 +0100

    aggiunte altre 5 righe di codice

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ età= input('inserisci la tua età ')
 DoveVivi=input('inserisci la città in cui abiti ')
 Fam=input('inserisci il numero di persone nella tua famiglia ')
 print(" ciao " + nome + " hai " + età+ " anni "  + " vivi a " + DoveVivi + " nella tua famiglia siete " + Fam )
+
+x=10
+b=4
+for i in range (5):
+    x+=2
+    b*=3
+print(x,b)
